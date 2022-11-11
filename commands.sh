537fe11c98d8b54b1a6fffff833df78ced34dc65
rusiruavb <rusiruavb98@gmail.com>
2022-11-12 00:08:48 +0530

    fix cpu utilization issue

--- commands.sh
+++ commands.sh
@@ -5,7 +5,7 @@ kubectl apply -f deployment.yaml
 kubectl apply -f hpa.yaml
 
 # Increase load
-kubectl run -i -tty load-generator --rm --image=busybox --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -q -O- http://calc-api; done"
+kubectl run -i --tty load-generator --rm --image=busybox:1.28 --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -q -O- http://calc-api; done"
 
 # Watch autoscaling
 kubectl get hpa calc-api --watch
