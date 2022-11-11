5ea51c82857b194567aa6579d711f3ad2c735641
rusiruavb <rusiruavb98@gmail.com>
2022-11-12 00:21:46 +0530

    update load generator command

--- commands.sh
+++ commands.sh
@@ -5,7 +5,7 @@ kubectl apply -f deployment.yaml
 kubectl apply -f hpa.yaml
 
 # Increase load
-kubectl run -i --tty load-generator --rm --image=busybox:1.28 --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -q -O- http://20.187.185.90/calc; done"
+kubectl run -i --tty load-generator --rm --image=busybox:1.28 --restart=Never -- /bin/sh -c "while sleep 0.01; do wget -q -O- 20.187.185.90/calc; done"
 
 # Watch autoscaling
 kubectl get hpa calc-api --watch
